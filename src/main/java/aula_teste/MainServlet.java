11c046791bfb2ea91a305de412e8ea2d615327ee
Laboratorio XXIA - 102 <lab102a@lab.unesc.net>
2022-03-22 21:28:30 -0300

    first commit

---
+++ src/main/java/aula_teste/MainServlet.java
@@ -0,0 +1,24 @@
+package aula_teste;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name= "Teste", urlPatterns = {"/", "/seila"})
+public class MainServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.print("TESTE");
+		System.out.println("Teste");
+	}
+	
+	
+
+}
